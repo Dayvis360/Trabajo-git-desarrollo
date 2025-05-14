40b0119c9ddb1aedf645edce53c052e3d6ec3c8b
Alejandro <alecocauwu@gmail.com>
2025-05-14 00:04:37 -0300

    agregue un saludo

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 document.writeln("hola mundo")
 document.writeln("Hola soy parte del grupo de desarrollo y me llamo David")
+document.writeln("Hola soy parte del grupo de desarrollo y me llamo Alejandro")
