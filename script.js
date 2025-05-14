a01384a6b132aec07c6a38a2ece0ae68e0999989
Lisette Montalvo <lisettmontalvorojas@gmail.com>
2025-05-14 01:03:01 -0300

    Saludo de Lisette agregado

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.writeln("hola mundo")
 document.writeln("Hola soy parte del grupo de desarrollo y me llamo David")
 document.writeln("Hola soy parte del grupo de desarrollo y me llamo Alejandro")
+document.writeln("Hola soy parte del grupo de desarrollo y me llamo Lisette")
